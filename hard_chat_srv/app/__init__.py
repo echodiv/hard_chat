78f0b9f5615b11333aff5f772b368e9a8b2f1051
Sergey Batalov <echodiv@outlook.com>
2020-05-11 01:18:25 +0300

    add orm, migrate and data scheme

--- hard_chat_srv/app/__init__.py
+++ hard_chat_srv/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import BaseConfig
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
 
-app.run(debug=True)
+#app.run(debug=True)
