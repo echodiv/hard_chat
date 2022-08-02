518d66fda68dae60daf8648a1c5ed32825804717
Sergey Batalov <echodiv@outlook.com>
2022-08-02 21:06:19 +0200

    set default logger

--- hardchat_srv/app/__init__.py
+++ hardchat_srv/app/__init__.py
@@ -1,12 +1,8 @@
 import logging
-import os
 from datetime import datetime
 
-from logging.handlers import RotatingFileHandler, SMTPHandler
-
-from config import BaseConfig
 from elasticsearch import Elasticsearch
-from flask import Flask, current_app, request, g
+from flask import Flask, current_app, g, request
 from flask_babel import Babel
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, current_user
@@ -16,7 +12,8 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from app.main.forms import SearchForm
-from app.urls import register_routers, register_error_handlers
+from app.urls import register_error_handlers, register_routers
+from config import BaseConfig
 
 # flask modules
 babel = Babel()
@@ -31,7 +28,7 @@ login.login_view = "auth.login"
 login.login_message = "Please log in to access this page"
 
 
-def set_middlewares(app: Flask, db) -> None:
+def set_middlewares(app: Flask, db: SQLAlchemy) -> None:
     @app.before_request
     def user_detector():
         if current_user.is_authenticated:
@@ -41,7 +38,7 @@ def set_middlewares(app: Flask, db) -> None:
         g.locale = str(get_locale())
 
 
-def create_app(config_class=BaseConfig):
+def create_app(config_class=BaseConfig) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config_class)
 
@@ -63,40 +60,8 @@ def create_app(config_class=BaseConfig):
     register_error_handlers(app)
     set_middlewares(app, db)
 
-    if not app.debug and not app.testing:
-        if app.config["MAIL_SERVER"]:
-            auth = None
-            if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
-                auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
-            secure = None
-            if app.config["MAIL_USE_TLS"]:
-                secure = ()
-            mail_handler = SMTPHandler(
-                mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
-                fromaddr="no-reply@" + app.config["MAIL_SERVER"],
-                toaddrs=app.config["ADMINS"],
-                subject="Microblog Failure",
-                credentials=auth,
-                secure=secure,
-            )
-            mail_handler.setLevel(logging.ERROR)
-            app.logger.addHandler(mail_handler)
-
-        if not os.path.exists("logs"):
-            os.mkdir("logs")
-        file_handler = RotatingFileHandler(
-            "logs/HARDCHAT.log", maxBytes=10240, backupCount=10
-        )
-        file_handler.setFormatter(
-            logging.Formatter(
-                "%(asctime)s %(levelname)s: %(message)s " "[in %(pathname)s:%(lineno)d]"
-            )
-        )
-        file_handler.setLevel(logging.DEBUG)
-        app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info("Microblog startup")
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("🚀 Microblog startup")
 
     return app
 
--- hardchat_srv/config.py
+++ hardchat_srv/config.py
@@ -13,15 +13,6 @@ class BaseConfig:
     MSG_PER_PAGE = int(os.environ.get("MSG_PER_PAGE") or 10)
     LANGUAGES = ["en", "ru"]
 
-    #  Flask-Mail #####
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = int(os.environ.get("") or 587)
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = MAIL_USERNAME
-    ADMINS = ["exzmple@mail.com"]
-
     #  Elasticsearch
     ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
 
