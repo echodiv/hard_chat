0f79e10285e16412b667bba1e8655a114d507ecf
Sergey Batalov <echodiv@outlook.com>
2022-08-02 21:40:41 +0200

    remove scripts folder

--- scripts/envoriment.sh
+++
@@ -1,7 +0,0 @@
-export FLASK_DEBUG=1
-export MAIL_SERVER=localhost
-export MAIL_PORT=8025
-export MAIL_USE_TLS=1
-export MAIL_USERNAME=admin@mail.com	
-export MAIL_PASSWORD=pwd
-export ELASTICSEARCH_URL=http://localhost:9200
